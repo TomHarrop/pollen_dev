d6db03f07a09e1f1d2d45840b5dd14e45cb380f9
Tom Harrop <twharrop@gmail.com>
2018-03-01 11:06:40 +1300

    logs

--- src/preprocess_gtf.R
+++ src/preprocess_gtf.R
@@ -6,18 +6,29 @@ library(rtracklayer)
 # GLOBALS #
 ###########
 
-gtf_file <- "data/ref/Araport11_GFF3_genes_transposons.201606.gtf"
-gff_file <- "data/ref/Araport11_GFF3_genes_transposons.201606.gff"
-cpus <- 8
-gtf_output <- paste0(
-    "output/010_ref/",
-    "Araport11_GFF3_genes_transposons_nuc_norrna.201606.gtf")
-# logs
+gtf_file <- snakemake@input[["gtf"]]
+gff_file <- snakemake@input[["gff"]]
+gtf_output <- snakemake@output[["gtf"]]
+cpus <- snakemake@threads[[1]]
+log_file <- snakemake@log[["log"]]
+
+
+# gtf_file <- "data/ref/Araport11_GFF3_genes_transposons.201606.gtf"
+# gff_file <- "data/ref/Araport11_GFF3_genes_transposons.201606.gff"
+# cpus <- 8
+# gtf_output <- paste0(
+#     "output/010_ref/",
+#     "Araport11_GFF3_genes_transposons_nuc_norrna.201606.gtf")
 
 
 ########
 # MAIN #
 ########
+
+# set log
+log <- file(log_file, open = "wt")
+sink(log, type = "message")
+sink(log, append = TRUE, type = "output")
 
 # set up multiprocessing
 BiocParallel::register(BiocParallel::MulticoreParam(cpus))
@@ -40,3 +51,6 @@ nuc_no_rRNA <- nuclear_gtf[!chuck]
 
 # write output
 export.gff(nuc_no_rRNA, gtf_output, format = "gtf")
+
+# write session info
+sessionInfo()
